83f8b6641d627ffbeb0d0565472d3486ace97f96
mihkelvaher <mihkel.vaher@industry62.com>
2022-01-17 17:23:26 +0200

    Tekitasin nupu ja lisasin talle CSS klassi

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Muuda <code>src/app.js</code> faili, et näha kirjutatut
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className='asdasdsa'>NUPP</button>
     </div>
   );
 }
